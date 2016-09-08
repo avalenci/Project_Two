0911990c172bc7171041e827c65727e22a5f3b8e
Andrew B Valenci <av128@pvhs-lab112-d05.pvsd.org>
2016-09-08 14:30:40 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String s;
+        s = "name";//initialize
+        String s2 = "nickname";
+        System.out.println(s);
+        //Naming rules of variables
+        //1. if, else, void, public...
+        //2. Can't start with number.
+        //3. ~!@#$%^&*()_+`-=/<>?:"{}|[]\
+        //4. No spaces.
+        //5. First Name not allowed.
+        //6. firstName first_name is allowed.
+        //7. Must be unique.
+        int a = 45;
+        double b =  3.0;
+        double sum = a + b;
+        a = 2_342_342;
+        int E, d = 0;
+        int sometime;
+        int thissssss = 0;
+        String samthing = "";
+        //Strings have methods.
+    }
+}
